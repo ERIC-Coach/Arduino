4c46069970f6400c7e2515702ef9dcbb60cb0c10
ERIC-Coach <ericbergelin@gmail.com>
2025-01-11 15:49:47 +0100

    Update KOB_02.ino

--- KOB_02.ino
+++ KOB_02.ino
@@ -15,7 +15,7 @@
   #define TG_PIN 9      // Pin TX pour la communication 4G
   #define RLED  12      // Témoin de transfert...
 
-  String myToken = "SGeozmBcVvWF4K7SQXQ6oAWnymZo1NXBIPy3pgSX9G3c4o3ZedHdoL7l_XZh-z-96CEdm-O4L2M-zE5sdB4bhA==";
+  String myToken = "kjhkjh";
   String myHost = "https://eu-central-1-1.aws.cloud2.influxdata.com";
   String myOrg = "b806d7e07da0b5e0";
 
@@ -32,14 +32,17 @@
 
   void setup()
   {
+  // Port série hardware pour debug (optionnel)
+    Serial.begin(115200);
+    delay(1000);
+
+    // start du port série débitmètre
+    rs485Serial.begin(9600);
+    delay(1000);
+    
     // start du port série 4G
     c4GSerial.begin(115200);
-    
-    // start du port série 485
-    rs485Serial.begin(9600);
-      
-    // Port série hardware pour debug (optionnel)
-    Serial.begin(115200);
+    delay(1000);
     
     // Configuration des pins RS485
     pinMode(RE_PIN, OUTPUT);
@@ -50,50 +53,60 @@
 
     pinMode(RLED, OUTPUT);
     digitalWrite(RLED, LOW);
+
+    delay(1000);      
   }
 
 
   void loop()
   {
-    if (rs485Serial. available()) {
-    String ReceivedData = rs485Serial.readStringUntil('\n');
-    Serial.println(ReceivedData);  // Attendu FLOW: 3.4567 m3/h
-    int twopoint = ReceivedData.indexOf(':');
-    int mletter = ReceivedData.indexOf('m'); // remplacer par l si on passe le débitmètre en l/s
-    sdebit =ReceivedData.substring(twopoint+1,mletter-1);
-    debit = sdebit.toFloat(); // ne garde que 2 décimales
-  }
-  Serial.println(debit);
-  if ((debit != savdebit)||(debit > 0)) // on envoie si le débit est différent du précédent envoyé/ ou > 0
-  {
-    savdebit = debit; 
-    digitalWrite(RLED, HIGH);
-      sendATCommand("AT",1000);
-      sendATCommand("AT+HTTPINIT",1000);
-    sendATCommand("AT+HTTPPARA=\"URL\",\"" + myHost + "/api/v2/write?org=" + myOrg + "&bucket=debitmetre&precision=s\"",1000);
-      sendATCommand("AT+HTTPPARA=\"USERDATA\",\"Authorization:Token " + myToken + "\"",1000);
-      sendATCommand("AT+HTTPPARA=\"CONTENT\",\"text/plain; charset=utf-8\"",1000);
-      taille = sdebit.length() + 33;
-      sendATCommand("AT+HTTPDATA=" + String(taille) + ",1000",1000); // là il faut la taille exacte
-      sendATCommand("debit,client=KOB,unit=m3/h debit=" + sdebit,1000);
-      digitalWrite(RLED, LOW);
-      sendATCommand("AT+HTTPACTION=1",3000); 
-      sendATCommand("AT+HTTPTERM",1000);
-  }
-  delay(500); 
-  }
-
-
-  String sendATCommand(String cmd, const int timeout) {
-    String response = "";
-    c4GSerial.println(cmd);
-    long int time = millis();
-    while((time+timeout) > millis()) {
-      while(c4GSerial.available()) {
-        char c = c4GSerial.read();
-        response += c;
-      }
+    rs485Serial.listen();
+    if (rs485Serial.isListening()){
+      Serial.println("Ecoute rs485Serial");
     }
-    Serial.println(response);
-    return response;
+    if (rs485Serial.available() > 0) 
+    {
+        String ReceivedData = rs485Serial.readStringUntil('\n');
+        Serial.println(ReceivedData);  // Attendu FLOW: 3.4567 m3/h
+        int twopoint = ReceivedData.indexOf(':');
+        int mletter = ReceivedData.indexOf('m'); // remplacer par l si on passe le débitmètre en l/s
+        sdebit =ReceivedData.substring(twopoint+2,mletter-1);
+        debit = sdebit.toFloat(); // ne garde que 2 décimales
+    }
+    Serial.println(sdebit);
+    if ((debit != savdebit)||(debit > 0.0)) // on envoie si le débit est différent du précédent envoyé/ ou > 0
+    {
+        c4GSerial.listen();
+        savdebit = debit; 
+        digitalWrite(RLED, HIGH);
+        sendATCommand("AT",1000);
+        sendATCommand("AT+HTTPINIT",1000);
+        sendATCommand("AT+HTTPPARA=\"URL\",\"" + myHost + "/api/v2/write?org=" + myOrg + "&bucket=debitmetre&precision=s\"",1000);
+        sendATCommand("AT+HTTPPARA=\"USERDATA\",\"Authorization:Token " + myToken + "\"",1000);
+        sendATCommand("AT+HTTPPARA=\"CONTENT\",\"text/plain; charset=utf-8\"",1000);
+        taille = sdebit.length() + 33;
+        sendATCommand("AT+HTTPDATA=" + String(taille) + ",1000",1000); // là il faut la taille exacte
+        sendATCommand("debit,client=KOB,unit=m3/h debit=" + sdebit,1000);
+        digitalWrite(RLED, LOW);
+        sendATCommand("AT+HTTPACTION=1",8000); 
+        sendATCommand("AT+HTTPTERM",1000);
+    }
+    delay(500); 
   }
+
+
+String sendATCommand(String cmd, const int timeout) {
+  Serial.println("sendATcommand :" + cmd);
+  String response = "";
+  c4GSerial.println(cmd);
+  long int time = millis();
+  while((time+timeout) > millis()) {
+    while(c4GSerial.available() > 0) {
+      char c = c4GSerial.read();
+      response += c;
+    }
+  }
+  Serial.println(response);
+  return response;
+}
+
